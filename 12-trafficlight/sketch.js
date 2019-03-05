88b73d320e6505f2de69fae7c676a30a1364de68
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-03-05 15:19:57 -0600

    update

--- 12-trafficlight/sketch.js
+++ 12-trafficlight/sketch.js
@@ -6,13 +6,21 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/
 
+let states;
+let timer;
+
 function setup() {
   createCanvas(600, 600);
+  state = 1;
+  
 }
 
 function draw() {
+  timer = millis() % 9000;
   background(255);
   drawOutlineOfLights();
+  determineState();
+  statesOfLights();
 }
 
 function drawOutlineOfLights() {
@@ -26,4 +34,58 @@ function drawOutlineOfLights() {
   ellipse(width/2, height/2 - 65, 50, 50); //top
   ellipse(width/2, height/2, 50, 50); //middle
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function determineState () {
+  if (state === 1 && timer >= 3000){
+  state = 2
+  console.log(millis)
+  }
+  
+  if (state === 2 && timer >= 6000){
+    state = 3
+  }
+  
+  if (state === 2 && timer <= 9000){
+    state = 1
+  }
+
+}
+
+
+function statesOfLights() {
+  if (state === 1){
+    fill(232, 20, 20);
+    ellipse(width/2, height/2 - 65, 50, 50); //top
+
+    fill (255);
+    ellipse(width/2, height/2, 50, 50); //middle
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+
+  }
+
+  if (state === 2){
+    fill(255);
+    ellipse(width/2, height/2 - 65, 50, 50); //top
+
+    fill (198, 147, 27);
+    ellipse(width/2, height/2, 50, 50); //middle
+
+    fill (255);
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+
+  }
+
+  if (state === 3){
+    fill(255);
+    ellipse(width/2, height/2 - 65, 50, 50); //top
+
+    fill (255);
+    ellipse(width/2, height/2, 50, 50); //middle
+
+    fill (97, 204, 40);
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+
+  }
+  
 }
