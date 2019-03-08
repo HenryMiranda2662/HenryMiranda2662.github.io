1d7587c510a88c74b443d6e797c08650f413ef07
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-03-08 15:20:03 -0600

    update game 2

--- states/sketch.js
+++ states/sketch.js
@@ -1,16 +1,16 @@
 // States
 // Henry Miranda Bastidas
-// March 4th, 2019
+// March th, 2019
 //
 // Extra for Experts:
 // Added music on the background
 
 // Set the variables that I am going to use for this project
 let backgroundImage;
-let x1, y1, r1;     
-let x2, y2, r2;     
-let x3;
-let x4;
+let playerX, playerY, playerRadius;     
+let enemyX, enemyY, enemyRadius;     
+let enemyX2;
+let enemyX3;
 let dy;             
 let gameOn;
 let button;
@@ -37,15 +37,15 @@ function setup(){
   gameOn = false // variable which allows me to check whether I am on the strat screen or gae screen
   
   //Variables used for the ball mechanis of the game such as coordinates and sizes
-  x1 = width/13;
-  y1 = height / 1.5;
-  x2 = 100;
-  y2 = height / 2;
-  x3 = width / 2;
-  x4 = 400;
-  dy = 6;
-  r1 = 30;
-  r2 = 25;
+  playerX = width/13;
+  playerY = height / 1.5;
+  enemyX = 100;
+  enemyY = height / 2;
+  enemyX2 = width / 2;
+  enemyX3 = 400;
+  dy = 2;
+  playerRadius = 30;
+  enemyRadius = 25;
 }
  
 
@@ -61,9 +61,9 @@ function draw() {
 
     //When the ball hits the rigth side of the screen, the game just resets
     // that way there are no winner or losers
-    if ((x1 + r1) >= 492){
+    if ((playerX + playerRadius) >= 492){
       gameOn = false;
-      }
+    }
   }
 
 // If the game has not started, draws a button which allows for the game to start
@@ -71,27 +71,26 @@ function draw() {
     background(66, 244, 119);  
     fill (250);
     rect(200, 225, 100 , 50);
-    x1 = width / 13; //Makes the ball start at the same x-value from the beginnig
+    playerX = width / 13; //Makes the ball start at the same x-value from the beginnig
     if (mouseIsPressed && mouseX > 200 && mouseX < 300 && mouseY > 225 && mouseY < 275){
       gameOn = true
     }
     image(buttonImage, 200, 225, 100, 50)
-  }
-  
+  } 
 }
 
 // Function which checks the distance between two balls, if close, it would count as a collition
 function itHit() {
 
-  distanceAway = int(dist(x1, y1, x2, y2));//checks distance of center of player ball and enemy ball
-  collition = (r1 + r2);                   // Adds radii of balls 
-  distanceAway2 = int(dist(x1, y1, x3, y2));
-  collition = (r1 + r2);
-  distanceAway3 = int(dist(x1, y1, x4, y2));
-  collition = (r1 + r2);
+  distanceAwayFromCenter = int(dist(playerX, playerY, enemyX, enemyY));//checks distance of center of player ball and enemy ball
+  collitionDistance = (playerRadius + enemyRadius);                   // Adds radii of balls 
+  distanceAwayFromCenter2 = int(dist(playerX, playerY, enemyX2, enemyY));
+  collitionDistance = (playerRadius + enemyRadius);
+  distanceAwayFromCenter3 = int(dist(playerX, playerY, enemyX3, enemyY));
+  collitionDistance = (playerRadius + enemyRadius);
   
   //if the distance between the centers of the balls is less than the radii added together, it is a collition
-  if (distanceAway  <= collition ||distanceAway2 <= collition || distanceAway3  <= collition)  {
+  if (distanceAwayFromCenter  <= collitionDistance ||distanceAwayFromCenter2 <= collitionDistance || distanceAwayFromCenter3  <= collitionDistance)  {
     gameOn = false
   }
 }
@@ -105,52 +104,59 @@ function playerBall () {
 
 function createPlayerBall(){
   fill(5, 255, 57)
-  ellipse(x1, y1, 60);
+  ellipse(playerX, playerY, 60);
 }
 
 // Moves the ball by chnaging x value of ball according to which arrow key is pressed
 function movePlayerBall(){
   if (keyIsDown(RIGHT_ARROW)) {
-    x1 += 4;
+    playerX += 4;
   }
   
   if (keyIsDown(LEFT_ARROW)) {
-    x1 -= 4;
+    playerX -= 4;
   }
 }
 
- //Checks if the ball is within the window boundary, if yes, the ball doesn't leave
+//Checks if the ball is within the window boundary, if yes, the ball doesn't leave
 function checkWindowBoundary() {
-  if ((x1 - r1) < 0) {
-    x1 = x1 + r1 / 3
+  if ((playerX - playerRadius) < 0) {
+    playerX = playerX + playerRadius / 3
   }
   
-  if ((x1 + r1) > 495) {
-    x1 = x1 - r1 / 4
+  if ((playerX + playerRadius) > 495) {
+    playerX = playerX - playerRadius / 4
   }
 }
-
 
 // Creates the enemy balls and their movement 
 function enemyBalls() {
-  
   fill(9, 150, 250)
-  
-  ellipse(x2, y2, r2 * 2);               // Creates the enemy ball
-  y2 += dy                               //Makes the ball move thoughout the y-axis
-  if (y2 + r2 >= width || y2 - r2 <= 0) {// check if the ball has crossed the screen boundary
+  enemyBall1();
+  enemyBall2();
+  enemyBall3();
+}
+
+function enemyBall1(){
+  ellipse(enemyX, enemyY, enemyRadius * 2);               // Creates the enemy ball
+  enemyY += dy                               //Makes the ball move thoughout the y-axis
+  if (enemyY + enemyRadius >= width || enemyY - enemyRadius <= 0) {// check if the ball has crossed the screen boundary
     dy = -1 * dy;                        //if so it changes its direction
   }
-  
-  ellipse(x3, y2, r2 * 2);
-  y2 += dy
-  if (y2 + r2 >= width || y2 - r2 <= 0) {
+}
+
+function enemyBall2(){
+  ellipse(enemyX2, enemyY, enemyRadius * 2);
+  enemyY += dy
+  if (enemyY + enemyRadius >= width || enemyY - enemyRadius <= 0) {
     dy = -1 * dy;
   }
-  
-  ellipse(x4, y2, r2 * 2);
-  y2 += dy
-  if (y2 + r2 >= width || y2 - r2 <= 0) {
+}
+
+function enemyBall3(){
+  ellipse(enemyX3, enemyY, enemyRadius * 2);
+  enemyY += dy
+  if (enemyY + enemyRadius >= width || enemyY - enemyRadius <= 0) {
     dy = -1 * dy;
   }
 }
