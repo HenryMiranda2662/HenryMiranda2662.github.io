4f0cc08e3719dcea39de475f89ec3e02fd5a7fb6
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-03-13 14:21:03 -0600

    update

---
+++ bouns line/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //background(0);
+}
+
+function draw() {
+  for (i = 10; i < 160; i += 10){
+    for (j = 10; j < 90; j += 10)
+    point(i,j)
+  }
+}
