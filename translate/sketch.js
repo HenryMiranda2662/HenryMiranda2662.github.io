8a5bb5cbbc9027aa431746fa157f3d28da00f954
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-03-25 15:19:48 -0600

    fff

---
+++ translate/sketch.js
@@ -0,0 +1,60 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x,y;
+let squareSize;
+let state;
+let speed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+}
+
+function draw(){
+  background(200);
+  translate(width/2,height/2);
+  ellipse(0, 0, 1, 1);
+  noFill();
+  ellipse(0, 0, 500, 500);
+  
+  fill("black")
+  rectMode(CENTER);
+  rect(0, -220, 10, 60);
+
+
+  for (i = 0, i > 11, i ++) {
+
+  }
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  rotate(30);
+  rect(0, -220, 10, 60);
+  
+  
+  
+
+
+}
