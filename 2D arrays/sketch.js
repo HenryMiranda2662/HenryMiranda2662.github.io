3b0acdc5e24f49ba6e8672452a5104577371f943
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-04-02 15:18:34 -0600

    2d array game of life

--- 2D arrays/sketch.js
+++ 2D arrays/sketch.js
@@ -5,12 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let gridSize = 10;
+let gridSize = 50;
 let grid;
 let cellSize;
 
 function setup() {
-  createCanvas (windowWidth, windowHeight);
+  if (windowWidth > windowHeight){
+  createCanvas (windowHeight, windowHeight);
+  }
+  else {
+    createCanvas (windowWidth, windowWidth);
+  }
   grid = createRandom2DArray (gridSize, gridSize);
   cellSize = width/gridSize;
 }
@@ -18,7 +23,6 @@ function setup() {
 function draw(){
   background(200);
   displayGrid();
-
 }
 
 function displayGrid() {
@@ -61,4 +65,72 @@ function createRandom2DArray(cols, rows) {
     }
   }
   return emptyArray;
+}
+
+
+function update(){
+  let nextTurn = create2DArray(gridSize, gridSize);
+
+  for (let y = 0; y < gridSize; y++){
+    for (let x = 0; x < gridSize; x++){
+      let neighboor = 0;
+
+      for (let i = -1; i <= 1; i++){
+        for (let j = -1; j <= 1; j++){
+          if(y+i >= 0 && y+i < gridSize && x+j >= 0 && x+j < gridSize ){
+          neighboor += grid[y+i][x+j];
+          }
+        }
+      }
+
+      neighboor -= grid[y][x];
+
+      if (grid[y][x] === 1 ){
+        if (neighboor === 2 || neighboor === 3){
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0){
+        if (neighboor === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+
+      
+      }
+
+    }
+  }
+
+  grid = nextTurn;
+
+}
+
+function keyPressed(){
+  if (key === " "){
+    update();
+  }
+
+  if (key === "r"){
+    grid = create2DArray(gridSize, gridSize);
+  }
+}
+
+function mousePressed(){
+  let xcoord = floor(mouseX/ cellSize);
+  let ycoord = floor(mouseY/ cellSize);
+  
+  
+  if (grid[ycoord][xcoord] === 1){
+    grid[ycoord][xcoord] = 0;
+  }
+  else {
+    grid[ycoord][xcoord] = 1;
+  }
 }
