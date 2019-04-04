29208e9f0e6964b46189ed37e1e942523e14f951
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-04-04 14:50:08 -0600

    2d array start project

---
+++ 2d-array/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x,y;
+let squareSize;
+let state;
+let speed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(220);
+ 
+}
